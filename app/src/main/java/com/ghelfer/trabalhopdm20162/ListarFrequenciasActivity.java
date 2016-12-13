5d0fea522eb033fbd21d32d6614b0cdf057335a8
Guilherme Oliveira <htoguilherme@gmail.com>
2016-12-13 15:30:48 -0200

    Novas listagens

--- app/src/main/java/com/ghelfer/trabalhopdm20162/ListarFrequenciasActivity.java
+++ app/src/main/java/com/ghelfer/trabalhopdm20162/ListarFrequenciasActivity.java
@@ -3,6 +3,13 @@ package com.ghelfer.trabalhopdm20162;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
@@ -13,7 +20,9 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -21,8 +30,18 @@ public class ListarFrequenciasActivity extends AppCompatActivity {
 
     final String URL_POST_FREQUENCIA = "http://ghelfer.net/pdm/ListaFrequenciaAtividade.aspx";
     final String URL_GET_ATIVIDADES = "http://ghelfer.net/pdm/ListaAtividade.aspx";
+    final String URL_EDITAR_FREQUENCIA = "http://ghelfer.net/pdm/AtualizaFrequenciaAtividade.aspx";
+
+
+    private List<Map<String,String>> listaDeAtividades;
+    final ArrayList<String> listaIdsAtividades = new ArrayList<String>();
+    private List<String> listaFrequencias;
+
+    private ListView listViewFrequencias;
+    private CheckBox checkBoxFrequencia;
 
     String idpessoa;
+    String idAtividade;
 
     AsyncHttpClient client;
 
@@ -31,17 +50,39 @@ public class ListarFrequenciasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar_frequencias);
+        checkBoxFrequencia = (CheckBox) findViewById(R.id.checkBoxFrequencia);
 
         idpessoa = getIntent().getStringExtra("idpessoa");
+        listaDeAtividades = new ArrayList<>();
+        listaFrequencias = new ArrayList<>();
+        listViewFrequencias = (ListView) findViewById(R.id.listViewFrequenciasG6);
+        listViewFrequencias.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Map<String,String> map = listaDeAtividades.get(position);
+                EditText txtTitulo = (EditText) findViewById(R.id.txtTituloAtividade);
+                txtTitulo.setText(map.get("titulo"));
+                idAtividade = listaIdsAtividades.get(position);
+                switch(map.get("frequencia")){
+                    case "true":
+                        checkBoxFrequencia.setChecked(true);
+                        break;
+                    default:
+                        checkBoxFrequencia.setChecked(false);
+                        break;
+                }
+            }
+        });
 
-        RequestParams params = new RequestParams();
-        params.add("idpessoa", idpessoa);
+
 
         client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.add("idpessoa", idpessoa);
         client.post(URL_POST_FREQUENCIA, params, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                ArrayList<String> listaIdsAtividades = new ArrayList<String>();
+
                 try {
                     String data = new String(responseBody);
                     JSONObject res = new JSONObject(data);
@@ -54,35 +95,52 @@ public class ListarFrequenciasActivity extends AppCompatActivity {
 
                     for(int i = 0;i<jsonArray.length();i++){
                         JSONObject obj = jsonArray.getJSONObject(i);
-                        Toast.makeText(ListarFrequenciasActivity.this, obj.getString("idatividade"), Toast.LENGTH_SHORT).show();
                         listaIdsAtividades.add(obj.getString("idatividade"));
+                        listaFrequencias.add(obj.getString("frequencia"));
                     }
 
                 } catch (Exception ex){
                     Log.e("ERRO", ex.getMessage());
                 }
 
-                for(String id : listaIdsAtividades){
-                    client.get(URL_GET_ATIVIDADES, new AsyncHttpResponseHandler() {
-                        @Override
-                        public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                            try {
-                                String data = new String(responseBody);
-                                JSONObject res = new JSONObject(data);
-                                JSONArray jsonArray = res.getJSONArray("Atividade");
-                                JSONObject obj = jsonArray.getJSONObject(0);
-                                Toast.makeText(ListarFrequenciasActivity.this,obj.getString("titulo") , Toast.LENGTH_SHORT).show();
-                            } catch (Exception ex){
-                                Log.e("ERRO", ex.getMessage());
+                RequestParams params = new RequestParams();
+                client.get(URL_GET_ATIVIDADES, new AsyncHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                        try {
+                            String data = new String(responseBody);
+                            JSONObject res = new JSONObject(data);
+                            JSONArray jsonArray = res.getJSONArray("Atividade");
+                            for(int i = 0;i<jsonArray.length();i++){
+                                JSONObject obj = jsonArray.getJSONObject(i);
+                                HashMap<String,String> map = new HashMap<String, String>();
+                                map.put("titulo", obj.getString("titulo"));
+                                map.put("local", obj.getString("local"));
+                                for(String id : listaIdsAtividades){
+                                    if(obj.getString("idatividade").equals(id)){
+                                        map.put("frequencia", listaFrequencias.get(i));
+                                        listaDeAtividades.add(map);
+                                    }
+                                }
                             }
-                        }
 
-                        @Override
-                        public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                            SimpleAdapter adapter = new SimpleAdapter(getBaseContext(),
+                                    listaDeAtividades,
+                                    R.layout.lista_cell_atividade,
+                                    new String[] {"titulo", "local", "frequencia"},
+                                    new int[] {R.id.itemTitulo, R.id.itemLocal, R.id.itemFrequencia});
+                            listViewFrequencias.setAdapter(adapter);
 
+                        } catch (Exception ex){
+                            Log.e("ERRO", ex.getMessage());
                         }
-                    });
-                }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                    }
+                });
 
             }
 
@@ -94,4 +152,34 @@ public class ListarFrequenciasActivity extends AppCompatActivity {
 
 
     }
+
+    protected void onBtnSalvarClicked(View v){
+        TextView txt = (TextView) findViewById(R.id.txtTituloAtividade);
+        if(txt.getText().toString().isEmpty()){
+            Toast.makeText(this, "Você precisa selecionar uma atividade para editar", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        txt.setText("");
+        client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("idinscricaoatividade", idAtividade);
+        if(checkBoxFrequencia.isChecked()){
+            params.put("frequencia", true);
+        } else {
+            params.put("frequencia", false);
+        }
+
+        client.post(URL_EDITAR_FREQUENCIA, params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                Toast.makeText(ListarFrequenciasActivity.this, "Frequencia editada com sucesso", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Toast.makeText(ListarFrequenciasActivity.this, "Ocorreu um erro, tente novamente", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/ghelfer/trabalhopdm20162/ListarPagamentosActivity.java
+++ app/src/main/java/com/ghelfer/trabalhopdm20162/ListarPagamentosActivity.java
@@ -2,12 +2,176 @@ package com.ghelfer.trabalhopdm20162;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
 
 public class ListarPagamentosActivity extends AppCompatActivity {
+
+    private final String URL_LISTA_PAGAMENTO = "http://ghelfer.net/pdm/ListaPagamentoEvento.aspx";
+    private final String URL_LISTA_EVENTO = "http://ghelfer.net/pdm/ListaEvento.aspx";
+
+    AsyncHttpClient client;
+
+    ArrayList<String> listaIdEventos;
+    ArrayList<String> listaPagamentos;
+    ArrayList<HashMap<String,String>> listaDeEventos;
+
+    ListView listViewPagamentos;
+
+    private String ID_PESSOA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listar_pagamentos);
+
+        listViewPagamentos = (ListView) findViewById(R.id.listViewPagamentos);
+        listaIdEventos = new ArrayList<>();
+        listaPagamentos = new ArrayList<>();
+        listaDeEventos = new ArrayList<>();
+        client = new AsyncHttpClient();
+        ID_PESSOA = getIntent().getStringExtra("idpessoa");
+
+        RequestParams params = new RequestParams();
+        params.put("idpessoa", ID_PESSOA);
+
+        client.post(URL_LISTA_PAGAMENTO, params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                try{
+                    String data = new String(responseBody);
+                    JSONObject res = new JSONObject(data);
+                    JSONArray array = res.getJSONArray("Pagamento");
+                    if(array.length() < 1){
+                        Toast.makeText(ListarPagamentosActivity.this, "Usuário não possui pagamentos", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+
+                    for(int i = 0;i < array.length();i++){
+                        JSONObject obj = array.getJSONObject(i);
+                        Log.d("DEBUG", obj.getString("idevento"));
+                        listaIdEventos.add(obj.getString("idevento"));
+                        listaPagamentos.add(obj.getString("pagamento"));
+
+                    }
+                } catch (Exception ex){
+                    Log.d("LOG_KEYS", ex.getMessage());
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Toast.makeText(ListarPagamentosActivity.this, "Erro, por favor, tente novamente mais tarde.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        client = new AsyncHttpClient();
+        client.post(URL_LISTA_EVENTO, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                try{
+                    String data = new String(responseBody);
+                    JSONObject res = new JSONObject(data);
+                    JSONArray array = res.getJSONArray("Evento");
+                    int indexPagamento = 0;
+
+                    for(int i = 0;i < array.length();i++) {
+                        JSONObject obj = array.getJSONObject(i);
+                        String idEvento = obj.getString("idevento");
+                        //Toast.makeText(ListarPagamentosActivity.this,"IDevento:" +  idEvento, Toast.LENGTH_SHORT).show();
+                    }
+
+
+                    /*for(int i = 0; i < listaIdEventos.size();i++){
+                        Toast.makeText(ListarPagamentosActivity.this, listaIdEventos.get(i), Toast.LENGTH_SHORT).show();
+                    }*/
+
+                        for(int x = 0;x<listaIdEventos.size();x++){
+                            Toast.makeText(ListarPagamentosActivity.this, "ID Q EU FUI: " + listaIdEventos.get(x), Toast.LENGTH_SHORT).show();
+
+                            for(int i = 0;i < array.length();i++) {
+                                try {
+                                    JSONObject obj = array.getJSONObject(i);
+                                    String idEvento = obj.getString("idevento");
+                                    String titulo = obj.getString("titulo");
+                                    Toast.makeText(ListarPagamentosActivity.this, "Evento na web " + idEvento, Toast.LENGTH_SHORT).show();
+                                    if(idEvento == listaIdEventos.get(x)){
+                                        HashMap<String,String> map = new HashMap<String, String>();
+
+                                        //TODO (>^~^)> † AQUI TEM UM BUG DO CAPIROTO † <(^~^<)
+                                        //TODO CHAMEM OS HORSES, É A HORA
+                                        map.put("titulo", titulo);
+                                        map.put("pagamento", "true");
+
+                                        listaDeEventos.add(map);
+                                        Toast.makeText(ListarPagamentosActivity.this, "Antes do hashmap", Toast.LENGTH_SHORT).show();
+
+                                        Toast.makeText(ListarPagamentosActivity.this, "Eror no if", Toast.LENGTH_SHORT).show();
+                                    }
+                                } catch (Exception ex) {
+
+                            }
+                        }
+                        /*
+                        for(String idEventoUsuario : listaIdEventos){
+                            if(idEvento.equals(idEventoUsuario)){
+                                HashMap<String,String> map = new HashMap();
+
+                                map.put("titulo", obj.getString("titulo"));
+                                //map.put("pagamento", listaPagamentos.get(indexPagamento));
+                                map.put("pagamento", "True");
+                                indexPagamento++;
+                                listaDeEventos.add(map);
+                            }
+                        }
+                        Toast.makeText(ListarPagamentosActivity.this, idEvento, Toast.LENGTH_SHORT).show();*/
+                    }
+
+
+
+
+                } catch (Exception ex){
+                    Toast.makeText(ListarPagamentosActivity.this, "Erro " + ex.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+                SimpleAdapter adapter = new SimpleAdapter(getBaseContext(),
+                        listaDeEventos,
+                        R.layout.lista_cell_evento,
+                        new String[] {"titulo", "pagamento"},
+                        new int[] {R.id.itemTituloEvento, R.id.itemPagamento});
+                listViewPagamentos.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
+
+
+
+
+    }
+
+
+    protected void onBtnAtualizarPagamento(View v){
+        Toast.makeText(this, "TODO", Toast.LENGTH_SHORT).show();
     }
 }
